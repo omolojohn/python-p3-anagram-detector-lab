da9656b507f589e99ff796258e70677ffc64627e
omolo-j <omoloj135@gmail.com>
2024-05-24 12:29:21 +0300

    anagram-detector-lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,8 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        sorted_word = sorted(self.word)
+        return [word for word in word_list if sorted(word) == sorted_word]
